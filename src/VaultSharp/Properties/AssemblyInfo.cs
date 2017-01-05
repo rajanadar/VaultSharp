d8414fff974b6ce80b6581949ab6dded38fc2103
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-01-06 09:23:11 +1100

    Strong name assemblies

--- src/VaultSharp/Properties/AssemblyInfo.cs
+++ src/VaultSharp/Properties/AssemblyInfo.cs
@@ -34,5 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.6.1.0")]
 [assembly: AssemblyFileVersion("0.6.1.0")]
-[assembly: InternalsVisibleTo("VaultSharp.UnitTests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("VaultSharp.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100836503b6fbdcacad7b7e8803d511760b60c31b79dc9ecbeb23b792aa1233182b38ebd92c90ab8c16399d5e723f3a4dc5dc559e9ab2a87272811bd9e6d7d97eb7aa31a258059ac44368d93191ffc13e65852029e375046d573d0c9f9443299284a425577acc2809cb9d57c1558db6a28b62bab8eefb0631556eb157bd0ec29289")]
