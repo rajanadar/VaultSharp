098a3ee382abcc9e1041b573d88c7be2f8b8a363
Raja Nadar <rajanadar@gmail.com>
2017-01-10 23:18:16 -0800

    missing field. fixes #18

--- src/VaultSharp/Backends/Authentication/Models/Token/CallingTokenInfo.cs
+++ src/VaultSharp/Backends/Authentication/Models/Token/CallingTokenInfo.cs
@@ -24,5 +24,14 @@ namespace VaultSharp.Backends.Authentication.Models.Token
         /// </value>
         [JsonProperty("explicit_max_ttl")]
         public int ExplicitMaximumTimeToLive { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="CallingTokenInfo"/> is renewable.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if renewable; otherwise, <c>false</c>.
+        /// </value>
+        [JsonProperty("renewable")]
+        public bool Renewable { get; set; }
     }
 }
