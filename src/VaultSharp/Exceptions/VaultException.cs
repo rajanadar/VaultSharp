ab143c509ab70f77a8b0b5ddc00725e4d98bc259
vk <vk@alphacloud.net>
2017-10-23 13:25:45 -0400

    Throw custom exception on Vault Http request failure.
    Return Http status code and response in exception properties.

---
+++ src/VaultSharp/Exceptions/VaultException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace VaultSharp.Exceptions
+{
+    /// <summary>
+    ///     Base exception class for Vault client errors.
+    /// </summary>
+    public class VaultException : Exception
+    {
+        /// <inheritdoc />
+        protected VaultException()
+        {
+        }
+
+        /// <inheritdoc />
+        protected VaultException(string message) : base(message)
+        {
+        }
+
+        /// <inheritdoc />
+        public VaultException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <inheritdoc />
+        protected VaultException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/VaultSharp/Exceptions/VaultRequestFailedException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+
+namespace VaultSharp.Exceptions
+{
+    /// <summary>
+    ///     Error making request to Vault server.
+    /// </summary>
+    public class VaultRequestFailedException : VaultException
+    {
+        /// <summary>
+        ///     Initializes a new instance of <see cref="VaultRequestFailedException" /> class.
+        /// </summary>
+        /// <param name="message">User message.</param>
+        /// <param name="statusCode">Http code.</param>
+        /// <param name="response">Server response.</param>
+        public VaultRequestFailedException(string message, HttpStatusCode statusCode, string response) : base(message)
+        {
+            StatusCode = statusCode;
+            Response = response ?? string.Empty;
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of <see cref="VaultRequestFailedException" /> class.
+        /// </summary>
+        /// <param name="message">User message.</param>
+        /// <param name="statusCode">Http code.</param>
+        /// <param name="response">Server response.</param>
+        /// <param name="innerException">Inner exception.</param>
+        public VaultRequestFailedException(string message, HttpStatusCode statusCode, string response,
+            Exception innerException) : base(message, innerException)
+        {
+            StatusCode = statusCode;
+            Response = response;
+        }
+
+        /// <summary>
+        ///     Http code returned by Vault server.
+        /// </summary>
+        public HttpStatusCode StatusCode { get; protected set; }
+
+        /// <summary>
+        ///     Vault server response.
+        /// </summary>
+        public string Response { get; protected set; }
+    }
+}
