81ed46233943b57b54452849fbcc8ac568eddb33
Raja Nadar <rajanadar@gmail.com>
2017-12-20 11:33:32 -0800

    temp

---
+++ src/VaultSharp/Backends/ListInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace VaultSharp.Backends
+{
+    /// <summary>
+    /// Represents the list of keys.
+    /// </summary>
+    public class ListInfo
+    {
+        /// <summary>
+        /// Gets or sets the keys.
+        /// </summary>
+        /// <value>
+        /// The keys.
+        /// </value>
+        [JsonProperty("keys")]
+        public List<string> Keys { get; set; }
+    }
+}
---
+++ src/VaultSharp/Backends/System/RenewedLease.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace VaultSharp.Backends.System
+{
+    /// <summary>
+    /// Renewed Lease info.
+    /// </summary>
+    public class RenewedLease
+    {
+        /// <summary>
+        /// Gets or sets the lease identifier.
+        /// </summary>
+        [JsonProperty("lease_id")]
+        public string LeaseId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the flag indicating if this lease is renewable.
+        /// </summary>
+        [JsonProperty("renewable")]
+        public bool Renewable { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time to live for the lease.
+        /// </summary>
+        [JsonProperty("lease_duration")]
+        public int LeaseDuration { get; set; }
+    }
+}
