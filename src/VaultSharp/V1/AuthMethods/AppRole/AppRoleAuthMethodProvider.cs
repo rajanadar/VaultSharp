6f8adf82e7e60266382c62c29833ee30bfeb9a22
Srigovind Nayak <sgovind.dev@outlook.com>
2021-10-30 12:48:55 +0530

    feat (Auth) : Added access AppRole Auth HTTP APIs (#226)
    * feat (Auth) : Added API to access ApprRole API
    - Updated interface for IAppRoleAuthMethod
    - Added AppRoleAuthMethodProvider class with implementation to look up role information
    - Added AppRoleInfo model
    - Registered AppRoleAuthMethodProvider to AuthMethodProvider
    
    * removed internal set
    
    * fix(AppRoleAuthMethodProvider) : default mountPoint fetched from AuthMethodDefaultPaths
    
    * fix (AppRoleInfo) : updated TTL to TimeToLive consistent with other parts of VaultSharp
    
    * chore : update changelog

---
+++ src/VaultSharp/V1/AuthMethods/AppRole/AppRoleAuthMethodProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using VaultSharp.Core;
+using VaultSharp.V1.AuthMethods.AppRole.Models;
+using VaultSharp.V1.Commons;
+
+namespace VaultSharp.V1.AuthMethods.AppRole
+{
+    internal class AppRoleAuthMethodProvider : IAppRoleAuthMethod
+    {
+        private readonly Polymath _polymath;
+
+        public AppRoleAuthMethodProvider(Polymath polymath)
+        {
+            Checker.NotNull(polymath, "polymath");
+            this._polymath = polymath;
+        }
+
+        public async Task<Secret<AppRoleInfo>> ReadRoleAsync(string roleName, string mountPoint = AuthMethodDefaultPaths.AppRole) 
+        {
+            Checker.NotNull(mountPoint, "mountPoint");
+            Checker.NotNull(roleName, "roleName");
+            return await _polymath.MakeVaultApiRequest<Secret<AppRoleInfo>>("v1/auth/"+ mountPoint.Trim('/')+"/role/"+roleName.Trim('/'), HttpMethod.Get).ConfigureAwait(_polymath.VaultClientSettings.ContinueAsyncTasksOnCapturedContext);
+        }
+    }
+}
