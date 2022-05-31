fbc338466317a6c84b143de59576e08acf03b9c8
Srigovind Nayak <sgovind.dev@outlook.com>
2022-05-31 14:13:13 +0530

    feat : added create, read, list & delete token role APIs (#246)
    * feat : added create, read, list & delete token role APIs
    
    * feat : added LIST HttpMethod
    
    * fix : separated create role request from model
    
    - set token type to service, which is the default
    
    * Revert "feat : added LIST HttpMethod"
    
    This reverts commit be5b24871db2f7d936cf0c6156158692d6476696.
    
    * fix : make list token role api consistent with other list apis

---
+++ src/VaultSharp/V1/AuthMethods/Token/ListTokenRoles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace VaultSharp.V1.AuthMethods.Token
+{
+    public class ListTokenRoles
+    {
+        /// <summary>
+        /// List of available token roles.
+        /// </summary>
+        [JsonProperty("keys")]
+        public List<string> Keys { get; set; }
+    }
+}
