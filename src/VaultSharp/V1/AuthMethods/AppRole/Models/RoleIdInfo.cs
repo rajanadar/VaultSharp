b23f526d768a4d18afb8a95c519fbd3e109e8153
Srigovind Nayak <sgovind.dev@outlook.com>
2022-10-27 23:15:55 +0530

    feat(gh-280): get role-id and pull secret-id for approle auth method

---
+++ src/VaultSharp/V1/AuthMethods/AppRole/Models/RoleIdInfo.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace VaultSharp.V1.AuthMethods.AppRole.Models
+{
+    /// <summary>
+    /// https://developer.hashicorp.com/vault/api-docs/auth/approle#read-approle-role-id
+    /// </summary>
+    public class RoleIdInfo
+    {
+        [JsonProperty("role_id")]
+        public int RoleId { get; set; }
+    }
+}
---
+++ src/VaultSharp/V1/AuthMethods/AppRole/Models/SecretIdInfo.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace VaultSharp.V1.AuthMethods.AppRole.Models
+{
+    /// <summary>
+    /// https://developer.hashicorp.com/vault/api-docs/auth/approle#generate-new-secret-id
+    /// </summary>
+    public class SecretIdInfo
+    {
+        [JsonProperty("secret_id_accessor")]
+        public string SecretIdAccessor { get; set; }
+        [JsonProperty("secret_id")]
+        public string SecretId { get; set; }
+        [JsonProperty("secret_id_ttl")]
+        public int SecretIdTimeToLive { get; set; }
+        [JsonProperty("secret_id_num_uses")]
+        public int SecretIdNumUses { get; set; }
+    }
+}
