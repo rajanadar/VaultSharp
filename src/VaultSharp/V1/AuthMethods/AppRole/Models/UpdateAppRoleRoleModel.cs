3d25ac428870d6b20a06c9d10bab20d0743bce14
Benjamin Bartels <benjamin@bartels.dev>
2026-08-17 04:18:30 +0100

    Apply suggestion from @bbartels

--- src/VaultSharp/V1/AuthMethods/AppRole/Models/UpdateAppRoleRoleModel.cs
+++ src/VaultSharp/V1/AuthMethods/AppRole/Models/UpdateAppRoleRoleModel.cs
@@ -7,6 +7,6 @@ namespace VaultSharp.V1.AuthMethods.AppRole.Models
     {
         [JsonIgnore]
         [Obsolete("LocalSecretIds can only be set when creating an app role. Use AppRoleRoleModel if creating a new app role", true)]
-public override bool LocalSecretIds { get; set; }
+        public override bool LocalSecretIds { get; set; }
     }
 }
